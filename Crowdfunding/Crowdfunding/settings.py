39700af2c54481d102c91b4066af7920355114d7
Carlos Daniel Parra <cdanielpg@outlook.com>
2021-10-11 23:18:28 -0500

    ModelosUP2

--- Crowdfunding/Crowdfunding/settings.py
+++ Crowdfunding/Crowdfunding/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'application.mipais'
+    'mipais.apps.MipaisConfig',
 ]
 
 MIDDLEWARE = [
