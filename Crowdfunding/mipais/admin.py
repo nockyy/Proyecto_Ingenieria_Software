0dfb21c9c7a2ca8e0dc11e85ec40c024d400b6f0
Carlos Daniel Parra <cdanielpg@outlook.com>
2021-10-12 09:19:48 -0500

    Test1

--- Crowdfunding/mipais/admin.py
+++ Crowdfunding/mipais/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import TipoPersona, MedioPago, Estado, Persona, Proyecto, Donacion
 
 # Register your models here.
+
+
+admin.site.register(TipoPersona)
+admin.site.register(MedioPago)
+admin.site.register(Estado)
+admin.site.register(Persona)
+admin.site.register(Proyecto)
+admin.site.register(Donacion)
